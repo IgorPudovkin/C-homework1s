b588414ddf87c3cda3bbbc60d489d35b2280c4ce
Igor <denlandpud@gmail.com>
2023-02-16 18:29:34 +0300

    Task8_itog

---
+++ task 8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a1 = 2;
+while(a1 <= num){
+    Console.WriteLine($"{a1}");
+    a1 = a1+2;
+}
