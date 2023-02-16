376ca6b6a860994b63da43b7e0896677b1c993cf
Igor <denlandpud@gmail.com>
2023-02-16 16:51:47 +0300

    Task6_itog

---
+++ task 6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка)
+Console.Clear();
+Console.WriteLine("enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 1){
+    Console.WriteLine("the " + num + " is odd");
+}
+else{
+    Console.WriteLine("the " + num + " is even");
+}
