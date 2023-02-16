aa11ea38a74ac30c4b495ec14043499f3b4c3e15
Igor <denlandpud@gmail.com>
2023-02-16 16:13:36 +0300

    Task4_itog

---
+++ task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("enter three numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max){
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+Console.WriteLine("the langest of the three numbers ->" + max);
